359b117fb19f250048f34add65a2e9a5329dbaae
Mosi8 <mauriziomosi8@gmail.com>
2021-12-10 16:18:32 +0100

    finito

--- myscript.js
+++ myscript.js
@@ -1,12 +1,14 @@
+let element = document.querySelector('.container');
+
 // ciclo for 
 for (let i = 1; i <= 100; i++){
     if ( ( i%3 == 0 ) && ( i%5 == 0 ) ){
-        console.log('FizzBuzz')
+        element.innerHTML += '<div class="block verde">FizzBuzz</div>';
     }else if( i%3 == 0 ){
-        console.log('Fizz')
+        element.innerHTML += '<div class="block rosso">Fizz</div>';
     }else if( i%5 == 0 ){
-        console.log('Buzz')
+        element.innerHTML += '<div class="block blu">Buzz</div>';
     }else{
-        console.log(i);
+        element.innerHTML += '<div class="block">' + (i) + '</div>';
     };
 };
